0a2c9ee02e2c362d1a66794dc1f8d3ccd02d34af
Hawk <90756025+HawkHackers900@users.noreply.github.com>
2021-10-03 07:17:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ apt install figlet -y
 apt install cowsay -y
 apt install ruby -y
 gem install lolcat -y
+apt install man -y
 echo -e "\e[34m All The Required Packages Installed"
 printf "$stop"
 figlet "DONE"
