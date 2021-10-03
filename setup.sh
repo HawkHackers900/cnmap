7595d3a834b13e3d9aa5045a7a1ef2231964866c
Hawk <90756025+HawkHackers900@users.noreply.github.com>
2021-10-03 02:19:32 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ apt install cowsay -y
 apt install ruby -y
 gem install lolcat -y
 apt install man -y
+cd $HOME
+cd cnmap
+mkdir output
 echo -e "\e[34m All The Required Packages Installed"
 printf "$stop"
 figlet "DONE"
